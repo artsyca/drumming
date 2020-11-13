2a51f12adfa6c40545937cb734dd3314b733a5f1
Alexandros K <alexandros.kessaris@gmail.com>
2020-11-13 12:25:21 +0900

    Change root element of app for embedding

--- config/environment.js
+++ config/environment.js
@@ -29,6 +29,7 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
+    ENV.APP.rootElement = '#ember-drumming';
   }
 
   if (environment === 'test') {
@@ -45,6 +46,8 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
     // here you can enable a production-specific feature
+    ENV.rootURL = '/ember-drumming';
+    ENV.APP.rootElement = '#ember-drumming';
   }
 
   return ENV;
